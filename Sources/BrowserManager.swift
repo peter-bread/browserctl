290a99c9c4c228054d42ce8547f7c7f43a2d1697
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2026-07-16 00:39:10 +0100

    refactor(launch): move business logic out of command

--- Sources/BrowserManager.swift
+++ Sources/BrowserManager.swift
@@ -48,6 +48,59 @@ enum BrowserManager {
 
         print("Set the default browser to: \(browser.formatted(as: .full))")
     }
+
+    static func launch(browser: String?, url: String?, dryRun: Bool) async throws {
+        let target = try resolveBrowser(browser: browser)
+        try await openBrowser(target: target, url: url, dryRun: dryRun)
+    }
+
+    private static func resolveBrowser(browser: String?) throws -> Browser {
+        if let browser {
+            guard let requested = BrowserManager.all().matching(browser).first else {
+                throw BrowserError.noBrowserMatchesQuery(browser)
+            }
+            return requested
+        } else {
+            guard let `default` = BrowserManager.defaultBrowser() else {
+                throw BrowserError.noDefaultBrowser
+            }
+            return `default`
+        }
+    }
+
+    private static func openBrowser(target: Browser, url: String? = nil, dryRun: Bool = false)
+        async throws
+    {
+        // Customise how the browser is opened
+        //
+        // https://developer.apple.com/documentation/appkit/nsworkspace/openconfiguration
+        let config = NSWorkspace.OpenConfiguration()
+
+        if let url {
+            guard let url = URL(string: url) else {
+                throw BrowserError.couldNotConstructURL(url)
+            }
+
+            if dryRun {
+                print("Would open \(url) in \(target.formatted(as: .full))")
+                return
+            }
+
+            // https://developer.apple.com/documentation/appkit/nsworkspace#Opening-URLs
+            try await NSWorkspace.shared.open(
+                [url], withApplicationAt: target.url, configuration: config)
+
+        } else {
+
+            if dryRun {
+                print("Would open \(target.formatted(as: .full))")
+                return
+            }
+
+            // https://developer.apple.com/documentation/appkit/nsworkspace#Launching-and-Hiding-Apps
+            try await NSWorkspace.shared.openApplication(at: target.url, configuration: config)
+        }
+    }
 }
 
 // MARK: - Utils
